d6284b614df0746a0493c2dc3b97f6b6a87710be
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-02-10 16:25:23 +0100

    chore: scaffold editor canvas

---
+++ components/editor/editor-canvas.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export function EditorCanvas() {
+  return (
+    <div className='flex h-full w-full flex-col items-center justify-center overflow-hidden'>
+      <canvas className='h-full w-full' />
+    </div>
+  )
+}
--- components/editor/editor-view.tsx
+++ components/editor/editor-view.tsx
@@ -3,11 +3,13 @@
 import { FileMenu } from './file-menu'
 import { ProjectName } from './project-name'
 import { ExportMenu } from './export-menu'
+
 import { EditorHeader } from './editor-header'
+import { EditorCanvas } from './editor-canvas'
 
 export function EditorView() {
   return (
-    <div className='flex h-full w-full flex-row'>
+    <div className='relative flex h-full w-full flex-col'>
       <EditorHeader>
         <div className='flex flex-row items-center gap-4'>
           <FileMenu />
@@ -17,6 +19,9 @@ export function EditorView() {
           <ExportMenu />
         </div>
       </EditorHeader>
+      <div className='relative flex w-full flex-grow flex-col overflow-hidden'>
+        <EditorCanvas />
+      </div>
     </div>
   )
 }
