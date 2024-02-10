2885ddc32d1cd8c41af34cf275fbf6b9b794c6a6
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-02-10 15:44:52 +0100

    fix: supress hydratation warning cause by cn

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body
         className={cn('bg-background font-sans antialiased', fontSans.variable)}
       >
