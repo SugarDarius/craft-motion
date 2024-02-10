98cc64e9d14852966226b9cf61ab75e3746e389a
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-02-10 17:49:56 +0100

    chore: add fabricCanvasRef

--- components/editor/hooks/useEditor.ts
+++ components/editor/hooks/useEditor.ts
@@ -1,4 +1,6 @@
 import { useEffect, useRef } from 'react'
+import { Canvas } from 'fabric/fabric-impl'
+
 import { setupCanvas } from '@/lib/fabric'
 
 type UseEditorReturnType = {
@@ -7,9 +9,11 @@ type UseEditorReturnType = {
 
 export function useEditor(): UseEditorReturnType {
   const canvasRef = useRef<HTMLCanvasElement>(null)
+  const fabricCanvasRef = useRef<Canvas | null>(null)
 
   useEffect(() => {
     const canvas = setupCanvas({ targetCanvasRef: canvasRef })
+    fabricCanvasRef.current = canvas
 
     return (): void => {
       canvas.dispose()
