3ad5aca6a7fa69f5e6902a8535235b485fa2af2e
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-02-11 10:11:32 +0100

    refactor: empower suspense fallbacl

--- app/liveblocks-room.tsx
+++ app/liveblocks-room.tsx
@@ -12,8 +12,10 @@ function LiveblocksRoomSuspenseFallback() {
     <div className="flex h-screen w-screen flex-col items-center justify-center overflow-hidden bg-[url('/grid.svg')]">
       <div className='flex h-fit w-fit flex-row items-center gap-2'>
         <ReloadIcon className='animate-spin' />
-        <h2 className='scroll-m-0 text-3xl font-semibold tracking-tight'>
-          Craft Motion
+        <h2 className='scroll-m-0 text-5xl font-extrabold tracking-tight'>
+          <span className='bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent'>
+            Craft Motion
+          </span>
         </h2>
       </div>
     </div>
