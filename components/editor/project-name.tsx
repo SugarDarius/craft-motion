be5f64d61dac669fbe75438988127fe6aed371b0
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-02-11 09:53:06 +0100

    refactor: empower project name

--- components/editor/project-name.tsx
+++ components/editor/project-name.tsx
@@ -6,8 +6,10 @@ import { getRandomUsername } from '@/lib/random-names'
 export function ProjectName() {
   const projectName = useMemo(() => getRandomUsername(), [])
   return (
-    <h4 className='scroll-m-0 text-xl font-semibold tracking-tight'>
-      {projectName}
+    <h4 className='scroll-m-0 text-3xl font-extrabold tracking-tight'>
+      <span className='bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent'>
+        {projectName}
+      </span>
     </h4>
   )
 }
