b63be3894a8c1704e6744adf55eb8cf4be5d32b3
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-02-12 16:50:13 +0100

    chore: add spinner icon

--- app/liveblocks-room-suspense-fallback.tsx
+++ app/liveblocks-room-suspense-fallback.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import { ReloadIcon } from '@radix-ui/react-icons'
+import { SpinnerIcon } from '@/components/ui/spinner-icon'
 
 export function LiveblocksRoomSuspenseFallback() {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center overflow-hidden bg-[url('/grid.svg')]">
       <div className='flex h-fit w-fit flex-row items-center gap-2'>
-        <ReloadIcon className='animate-spin' />
+        <SpinnerIcon className='animate-spin' />
         <h2 className='scroll-m-0 text-5xl font-extrabold tracking-tight'>
           <span className='bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent'>
             Craft Motion
