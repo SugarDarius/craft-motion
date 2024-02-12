35a65e8edc67cd9b108268eae9730a5d62b37dc6
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-02-12 17:09:46 +0100

    chore: scaffold duration

--- components/editor/editor-inspector.tsx
+++ components/editor/editor-inspector.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+import { Label } from '@/components/ui/label'
+import { Slider } from '@/components/ui/slider'
+
 export function EditorInspector() {
   return (
     <div className='absolute bottom-0 right-8 top-0 z-10 mx-0 my-auto h-[80%] w-72'>
@@ -15,7 +18,25 @@ export function EditorInspector() {
             Animation
           </span>
         </div>
-        <div className='w-full flex-shrink-0 px-4 py-2'></div>
+        <div className='w-full flex-shrink-0 gap-2 px-4 py-2'>
+          <div className='grid gap-2'>
+            <div className='flex items-center justify-between'>
+              <Label htmlFor='duration'>Duration (s)</Label>
+              <span className='w-12 rounded-md border border-transparent px-2 py-0.5 text-right text-sm text-muted-foreground hover:border-border'>
+                {10}
+              </span>
+            </div>
+            <Slider
+              id='duration'
+              max={10}
+              // defaultValue={10}
+              step={1}
+              // onValueChange={setValue}
+              className='[&_[role=slider]]:h-4 [&_[role=slider]]:w-4'
+              aria-label='Maximum Length'
+            />
+          </div>
+        </div>
       </div>
     </div>
   )
