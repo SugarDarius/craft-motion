46556e9976efb7247143f07351a3ba0e737f2810
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-02-13 18:31:00 +0100

    refactor: update architecture

--- components/editor/editor-header.tsx
+++ components/editor/editor-header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export function EditorHeader({ children }: { children: React.ReactNode }) {
   return (
-    <div className='relative flex h-fit w-full flex-shrink-0 flex-row items-center px-4 py-6'>
+    <div className='absolute left-0 top-0 z-10 flex h-fit w-full flex-shrink-0 flex-row items-center px-[2rem] py-6'>
       {React.Children.map(children, (child) => {
         return <div className='flex h-fit w-fit flex-1'>{child}</div>
       })}
