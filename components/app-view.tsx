396cb7f516399631f55adbc2bb1e272afa0d61f7
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-02-13 11:22:52 +0100

    refactor: add grid for light color scheme

--- components/app-view.tsx
+++ components/app-view.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export function AppView({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen w-screen flex-col overflow-hidden bg-[url('/grid.svg')]">
+    <div className="flex h-screen w-screen flex-col overflow-hidden bg-[url('/grid-light.svg')] dark:bg-[url('/grid-dark.svg')]">
       {children}
     </div>
   )
