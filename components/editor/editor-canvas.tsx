aef3ce02a1b6718ebc508aaf348540698e867669
Aurélien Dupays Dexemple <dupaysdexemple.aurelien@outlook.com>
2024-02-14 15:45:41 +0100

    chore: add presence cursor tracking

--- components/editor/editor-canvas.tsx
+++ components/editor/editor-canvas.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import useEvent from 'react-use-event-hook'
+
 import {
   ContextMenu,
   ContextMenuContent,
@@ -7,6 +9,8 @@ import {
   ContextMenuShortcut,
   ContextMenuTrigger,
 } from '@/components/ui/context-menu'
+import { useUpdateMyPresence } from '@/liveblocks.config'
+import React from 'react'
 
 export function EditorCanvas({
   canvasRef,
@@ -21,12 +25,27 @@ export function EditorCanvas({
   canRedo: boolean
   onRedo: () => void
 }) {
+  const updateMyPresence = useUpdateMyPresence()
+
+  const handlePointerMove = useEvent((e: React.PointerEvent): void => {
+    updateMyPresence({
+      x: Math.round(e.clientX),
+      y: Math.round(e.clientY),
+    })
+  })
+
+  const handlePointerLeave = useEvent((): void => {
+    updateMyPresence({})
+  })
+
   return (
     <ContextMenu>
       <ContextMenuTrigger className='flex h-auto w-full flex-col'>
         <div
           id='canvas-box'
           className='flex h-full w-full flex-col items-center justify-center overflow-hidden'
+          onPointerMove={handlePointerMove}
+          onPointerLeave={handlePointerLeave}
         >
           <canvas ref={canvasRef} id='canvas' className='h-full w-full' />
         </div>
